d0912e7a0769bf195e58a0a8ad9d50195323c233
marioruizgonzalez <marioruizgonzalez.mx@gmail.com>
2022-03-06 17:37:30 -0600

    mrg||primera hoja con streamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#find more emojis here: https://wwww.webfx.com/tools/emojis-cheat-sheet/
+
+st.set_page_config(page_title="My Webpage", page_icon=":=tada", layout="wide")
+
+## ----------- HEADER SECTION ----------
+
+st.subheader("Hi, I am Mario Ruiz :wave:")
+st.title("A data analyst from Mexico")
+st.write("I am passionate about finding ways to  use Python and VBA to be more efficient")
